a666a4f793d055731476a29ff8cca10291f8537f
onsfadhli <onsfadhli6@gmail.com>
2025-12-14 04:23:42 +0100

    fix: finalize Flask app package and run config

--- app/__init__.py
+++ app/__init__.py
@@ -1,19 +1,10 @@
-import os
 from flask import Flask
-from config import config_by_name
 
-def create_app(config_name=None):
-    if config_name is None:
-        config_name = os.environ.get('FLASK_CONFIG') or 'default'
-
+def create_app():
     app = Flask(__name__)
-    app.config.from_object(config_by_name[config_name])
+    app.config.from_object("config")
 
-    # Import routes après création de l'app pour éviter circular import
-    from . import routes
-    routes.init_routes(app)  # Appelle la fonction qui enregistre les routes
+    from app.routes import main
+    app.register_blueprint(main)
 
     return app
-
-# Définir app pour Gunicorn
-app = create_app()
